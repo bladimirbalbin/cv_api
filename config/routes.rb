4aa41e69c593ef471274f4fd762b8c8f9bf797d5
bladimirbalbinCobelen <bladimir.balbin@cobelen.com>
2026-01-25 13:31:35 -0500

    refactor: root routes redirect api-docs

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   resources :users, only: [], param: :email do
     get :profile, on: :collection
   end
-
+  # Redirigir la raíz a la documentación de Swagger
+  root to: redirect('/api-docs')
   # Documentation Endpoints (Solo se definen UNA vez)
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
