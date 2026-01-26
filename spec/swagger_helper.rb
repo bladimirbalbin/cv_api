981cabd4e7ff07fb83aa5d5ff170a7f535b81374
bladimirbalbinCobelen <bladimir.balbin@cobelen.com>
2026-01-25 21:10:08 -0500

    docs: set production server as default in swagger ui

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -5,28 +5,7 @@ require "rails_helper"
 RSpec.configure do |config|
   config.openapi_root = Rails.root.join("public/swagger").to_s
 
-  servers =
-    if Rails.env.production?
-      [
-        {
-          url: "https://cv-api-xpe1.onrender.com",
-          description: "Production Server (Render)"
-        }
-      ]
-    else
-      [
-        {
-          url: "http://localhost:3000",
-          description: "Local Development Server"
-        },
-        {
-          url: "https://cv-api-xpe1.onrender.com",
-          description: "Production Server (Render)"
-        }
-      ]
-    end
-
-  config.openapi_specs = {
+    config.openapi_specs = {
     "v1/swagger.yaml" => {
       openapi: "3.0.0",
       info: {
@@ -34,7 +13,16 @@ RSpec.configure do |config|
         version: "v1"
       },
       paths: {},
-      servers: servers,
+      servers: [
+        {
+          url: "https://cv-api-xpe1.onrender.com",
+          description: "Production Server (Render)"
+        },
+        {
+          url: "http://localhost:3000",
+          description: "Local Development Server"
+        }
+      ],
       components: {
         securitySchemes: {
           bearer_auth: {
